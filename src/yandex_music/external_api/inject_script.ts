c8309e67fb435ecd87e898757843706aa98ad396
Golev Yakov <yavg.boiler@gmail.com>
2023-05-20 10:08:51 +0500

    path fix

--- src/yandex_music/external_api/inject_script.ts
+++ src/yandex_music/external_api/inject_script.ts
@@ -1,6 +1,6 @@
 /** Inject Script in page */
 export function injectScript() {
-    const file_path = chrome.runtime.getURL('build/inject_content.bundle.js') 
+    const file_path = chrome.runtime.getURL('inject_content.bundle.js') 
     var node = document.querySelector('body')
     var script = document.createElement('script')
     script.setAttribute('type', 'text/javascript')
