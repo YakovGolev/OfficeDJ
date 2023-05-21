f564127c3c5272c625923a41dba0d1eea75b5d9d
Golev Yakov <yavg.boiler@gmail.com>
2023-05-21 22:38:05 +0500

    add tracks to playlist

--- src/yandex_music/page_interaction/actions.ts
+++ src/yandex_music/page_interaction/actions.ts
@@ -1,20 +1,65 @@
 import { sendMessageAsync } from "telegram/messaging"
 import { navigate } from "yandex_music/external_api/actions"
-import { buildSidebarSelector, contextMenuButtonSelector, addToQueueButtonSelector, artistSelector } from "./selectors"
+import { buildSidebarSelector, contextMenuButtonSelector, addToPlaylistButtonSelector, artistSelector, listOfPlaylistsSelector, createPlaylistInputSelector } from "./selectors"
 import { waitForElementLoaded, clickButton, sleep } from "utility"
 
 const extensionPlaylist = 'Office DJ'
+let playlistsUrl : URL, extensionPlaylistUrl : URL
+
+const updatePlaylistsLink = () => {
+  const link = (document.querySelector('a[data-name=my]') as HTMLAnchorElement).href
+  if (link)
+    playlistsUrl = new URL(link)
+}
 
 /** Add track to extension playlist. */
 export const addTrackAsync = async (trackUrl: URL, chatId: number): Promise<void> => {
   navigate(trackUrl)
   await waitForElementLoaded(buildSidebarSelector(trackUrl.pathname))
   clickButton(contextMenuButtonSelector)
-  await waitForElementLoaded(addToQueueButtonSelector)
-  clickButton(addToQueueButtonSelector)
+  await waitForElementLoaded(addToPlaylistButtonSelector)
+  clickButton(addToPlaylistButtonSelector)
+  await waitForElementLoaded(listOfPlaylistsSelector)
+  const extensionPlaylistElement = getPlaylist()
+  extensionPlaylistElement?.click()
+  if (!extensionPlaylistElement) {
+    const input = document.querySelector(createPlaylistInputSelector) as HTMLInputElement
+    input.value = extensionPlaylist
+    const keyboardEvent = new KeyboardEvent('keyup', {
+      code: 'Enter',
+      key: 'Enter',
+      charCode: 13,
+      keyCode: 13,
+      view: window,
+      bubbles: true
+    })
+    input.dispatchEvent(keyboardEvent)
+  }
   await sleep(300)
   const trackName = (document.querySelector(buildSidebarSelector(trackUrl.pathname)) as HTMLElement)?.innerText
   const artistName = (document.querySelector(artistSelector) as HTMLElement)?.innerText
   const message = `Трек "${artistName} - ${trackName}" успешно добавлен в очередь воспроизведения`
   await sendMessageAsync(chatId, message)
+
+  if (!extensionPlaylistUrl){
+    if (!playlistsUrl)
+      updatePlaylistsLink()
+    navigate(playlistsUrl)
+    const playlistSelector = `div[title="${extensionPlaylist}"] a`
+    await waitForElementLoaded(playlistSelector)
+    const link = (document.querySelector(playlistSelector) as HTMLAnchorElement)?.href
+    if (link)
+      extensionPlaylistUrl = new URL(link)      
+  }
+  if (extensionPlaylistUrl)
+    navigate(extensionPlaylistUrl)
 }
+
+const getPlaylist = () => {
+  const playlistElement = document.querySelector(listOfPlaylistsSelector)?.querySelector(`span[title="${extensionPlaylist}"]`)
+  if (playlistElement)
+    return playlistElement.parentElement
+
+  return null
+}
+
--- src/yandex_music/page_interaction/selectors.ts
+++ src/yandex_music/page_interaction/selectors.ts
@@ -1,5 +1,7 @@
 export const contextMenuButtonSelector = '.sidebar-track .d-context-menu .d-button'
-export const addToQueueButtonSelector = '.d-context-menu__popup .d-context-menu__item_forth'
+export const addToPlaylistButtonSelector = '.d-context-menu__popup .d-context-menu__item_add'
+export const listOfPlaylistsSelector = '.d-context-menu__popup .d-addition__playlists'
 export const artistSelector = '.sidebar-track .sidebar__info .d-artists'
+export const createPlaylistInputSelector = '.d-context-menu__popup .d-addition__actions-visible input'
 
 export const buildSidebarSelector = (href: string) => `.sidebar-track .sidebar-track__title a[href="${href}"]`
